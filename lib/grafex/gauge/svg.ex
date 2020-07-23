cf86897e3a5bcd559d08eae5d32c0f31849611cd
Ondrej Tucek <ondrej.tucek@gmail.com>
2020-07-23 10:25:47 +0200

    Add gauge graph

--- lib/grafex/gauge/svg.ex
+++ lib/grafex/gauge/svg.ex
@@ -1,7 +1,7 @@
-defmodule Grafex.Gauge.Svg do
+defmodule Examples.Gauge.Svg do
   @moduledoc false
 
-  alias Grafex.Gauge
+  alias Examples.Gauge
 
   use Phoenix.HTML
 
@@ -24,22 +24,53 @@ defmodule Grafex.Gauge.Svg do
         <% end %>
       </g>
 
+      <g class="major-ticks-text">
+        <%= for {x, y, tick_value, text_anchor} <- @settings.major_ticks_text.positions do %>
+          <text class="major-text"
+            x="<%= x %>" y="<%= y %>"
+            text-anchor="<%= text_anchor %>"
+            ><%= tick_value %>
+          </text>
+        <% end %>
+      </g>
+
       <g class="gauge">
-        <path id="gauge-bg-border"
-          d="<%= @settings.d_gauge_half_circle %>">
-        </path>
+        <g class="gauge-bg-border-bottom-lines">
+          <path id="gauge-bg-border"
+            d="<%= @settings.d_gauge_half_circle %>">
+          </path>
+          <%= for lines <- @settings.d_gauge_bg_border_bottom_lines do %>
+            <path class="gauge-bg-border-bottom-lines"
+              d="<%= lines %>">
+            </path>
+          <% end %>
+        </g>
         <path id="gauge-bg"
           d="<%= @settings.d_gauge_half_circle %>">
         </path>
         <path id="gauge-value"
+          <%= if String.length(@settings.gauge_value_class) != 0 do %>
+            class="<%= @settings.gauge_value_class %>"
+          <% end %>
           d="<%= @settings.d_value %>">
         </path>
       </g>
 
-      <text class="value-text"
-        x="<%= elem(@settings.text_value_position, 0) %>" y = "<%= elem(@settings.text_value_position, 1) %>"
-        text-anchor="middle" alignment-baseline="middle" dominant-baseline="central">
-          <%= @settings.text_value %>
+      <%= if !Enum.empty?(@settings.thresholds.positions_with_class_name) do %>
+        <g class="thresholds">
+          <%= for {treshold, angle, class} <- @settings.thresholds.d_thresholds_with_class do %>
+            <path class="<%= class %>"
+              d="<%= treshold %>"
+              transform="rotate(<%= angle %>, <%= elem(@settings.gauge_center, 0) %>, <%= elem(@settings.gauge_center, 1) %>)">
+            </path>
+          <% end %>
+        </g>
+      <% end %>
+
+      <text class="value-font value-text"
+        x="<%= elem(@settings.value_text.position, 0) %>" y = "<%= elem(@settings.value_text.position, 1) %>"
+        text-anchor="middle" alignment-baseline="middle" dominant-baseline="central"
+        ><%= @settings.text_value %>
       </text>
 
     </svg>
