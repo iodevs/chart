67f2634a14a4672f191f87157198bcd31790973f
ondrej-tucek <ondrej.tucek@gmail.com>
2020-08-20 09:12:48 +0200

    Rename turn to visibility

--- lib/chart/internal/title.ex
+++ lib/chart/internal/title.ex
@@ -2,7 +2,7 @@ defmodule Chart.Internal.Title do
   @moduledoc false
 
   alias Chart.Internal.Text
-  import Chart.Internal.Guards, only: [is_turn: 1]
+  import Chart.Internal.Guards, only: [is_visibility: 1]
 
   @self_key :title
 
@@ -18,13 +18,13 @@ defmodule Chart.Internal.Title do
   end
 
   def set_rect_bg(settings, rect_bg)
-      when is_map(settings) and is_turn(rect_bg) do
+      when is_map(settings) and is_visibility(rect_bg) do
     put_in(settings, [@self_key, :rect_bg], rect_bg)
   end
 
-  def set_turn(settings, turn)
-      when is_map(settings) and is_turn(turn) do
-    put_in(settings, [@self_key, :turn], turn)
+  def set_visibility(settings, visibility)
+      when is_map(settings) and is_visibility(visibility) do
+    put_in(settings, [@self_key, :visibility], visibility)
   end
 
   def set_text(settings, text)
