835f3b261f49794ab082ee7109afb6785d0d9ef7
Ondrej Tucek <ondrej.tucek@gmail.com>
2020-08-24 21:34:54 +0200

    Add rectangle module

--- lib/chart/internal/plot.ex
+++ lib/chart/internal/plot.ex
@@ -1,22 +1,18 @@
 defmodule Chart.Internal.Plot do
   @moduledoc false
 
+  alias Chart.Internal.Rectangle
   alias Chart.Internal.AxisLine.{Label, MajorTicks, MajorTicksText, MinorTicks}
 
-  import Chart.Internal.Guards, only: [is_nonnegative_number: 1, is_positive_number: 1]
+  import Chart.Internal.Guards, only: [is_positive_number: 1]
 
   @self_key :plot
 
-  def new() do
-    %{
-      position: {100, 100},
-      rect_bg_padding: {0, 0, 0, 0},
-      size: {600, 400}
-    }
-  end
-
   def add(settings) when is_map(settings) do
-    Map.put(settings, @self_key, new())
+    settings
+    |> Rectangle.add(@self_key)
+    |> Rectangle.set_position(@self_key, {100, 100})
+    |> Rectangle.set_size(@self_key, {600, 400})
   end
 
   # Setters
@@ -24,8 +20,8 @@ defmodule Chart.Internal.Plot do
   def set_position(settings, {x, y} = position)
       when is_map(settings) and is_positive_number(x) and is_positive_number(y) do
     settings
-    |> put_in(
-      [@self_key, :position],
+    |> Rectangle.set_position(
+      @self_key,
       validate_position(position, settings.figure.viewbox, settings.plot.size)
     )
     |> MajorTicks.set_positions({1, 0})
@@ -38,21 +34,14 @@ defmodule Chart.Internal.Plot do
     |> Label.set_position({0, 1})
   end
 
-  def set_rect_bg_padding(settings, rect_bg_padding) do
-    put_in(
-      settings,
-      [@self_key, :rect_bg_padding],
-      validate_rect_bg_padding(rect_bg_padding)
-    )
+  def set_background_padding(settings, padding) do
+    Rectangle.set_padding(settings, @self_key, padding)
   end
 
   def set_size(settings, {x, y} = size)
       when is_map(settings) and is_positive_number(x) and is_positive_number(y) do
     settings
-    |> put_in(
-      [@self_key, :size],
-      validate_size(size, settings.figure.viewbox)
-    )
+    |> Rectangle.set_size(@self_key, validate_size(size, settings.figure.viewbox))
     |> MajorTicks.set_positions({1, 0})
     |> MajorTicks.set_positions({0, 1})
     |> MinorTicks.set_positions({1, 0})
@@ -72,12 +61,6 @@ defmodule Chart.Internal.Plot do
        )
        when pos_x + plot_width < fig_width and pos_y + plot_height < fig_height do
     position
-  end
-
-  defp validate_rect_bg_padding({top, right, bottom, left} = pad)
-       when is_nonnegative_number(top) and is_nonnegative_number(right) and
-              is_nonnegative_number(bottom) and is_nonnegative_number(left) do
-    pad
   end
 
   defp validate_size({width, height} = size, {fig_width, fig_height})
