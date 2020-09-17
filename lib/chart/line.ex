3ff979e00d93f10f6e86608d71c5e68aac43fefb
ondrej-tucek <ondrej.tucek@gmail.com>
2020-09-17 15:11:21 +0200

    Fix axis label

--- lib/chart/line.ex
+++ lib/chart/line.ex
@@ -2,8 +2,7 @@ defmodule Chart.Line do
   @moduledoc """
   A line chart definition structure
   """
-
-  alias Chart.Internal.{GridLine, Text}
+  alias Chart.Internal.{AxisLine, GridLine, Text}
   alias Chart.Internal.Storage.Buffer
   alias Chart.Internal.AxisLine.MajorTicksText
   alias Chart.Line.{Settings, Svg}
@@ -26,13 +25,35 @@ defmodule Chart.Line do
     ])
   end
 
-  #  Setters
+  #  Axis label setters
+
+  def set_axis_label_adjust_placement(%Chart{} = chart, axis, adjust_placement) do
+    settings = chart.settings |> AxisLine.Label.set_adjust_placement(axis, adjust_placement)
+
+    Chart.put_settings(chart, settings)
+  end
+
+  def set_axis_label(%Chart{} = chart, axis, text) do
+    settings = chart.settings |> AxisLine.Label.set_text(axis, text)
+
+    Chart.put_settings(chart, settings)
+  end
+
+  def set_axis_label_placement(%Chart{} = chart, axis, placement) do
+    settings = chart.settings |> AxisLine.Label.set_placement(axis, placement)
+
+    Chart.put_settings(chart, settings)
+  end
+
+  #  Grid setters
 
   def set_grid(%Chart{} = chart, axis_grid_type) do
     settings = chart.settings |> GridLine.set_grid(axis_grid_type)
 
     Chart.put_settings(chart, settings)
   end
+
+  #  Title setters
 
   def set_title_position(%Chart{} = chart, text) do
     settings = chart.settings |> Text.set_position(:title, text)
