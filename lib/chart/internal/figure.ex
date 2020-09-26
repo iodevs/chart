a7cd072df447892d2fc3f541ba713baa7ea30e40
Ondrej Tucek <ondrej.tucek@gmail.com>
2020-09-26 23:00:45 +0200

    Add svg responsivity

--- lib/chart/internal/figure.ex
+++ lib/chart/internal/figure.ex
@@ -4,7 +4,10 @@ defmodule Chart.Internal.Figure do
   import Chart.Internal.Guards, only: [is_positive_number: 1]
 
   def new() do
-    %{viewbox: {800, 600}}
+    %{
+      viewbox: {800, 600},
+      preserve_aspect_ratio: "xMidYMid meet"
+    }
   end
 
   def add(settings) when is_map(settings) do
@@ -14,5 +17,9 @@ defmodule Chart.Internal.Figure do
   def set_viewbox(settings, {width, height} = viewbox)
       when is_positive_number(width) and is_positive_number(height) do
     put_in(settings, [:figure, :viewbox], viewbox)
+  end
+
+  def set_aspect_ratio(settings, par) when is_map(settings) and is_binary(par) do
+    put_in(settings, [:figure, :preserve_aspect_ratio], par)
   end
 end
